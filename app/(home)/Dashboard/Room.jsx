db92e1e8c3f84742bdd796772d65aa042dcd9596
sanmay06 <sanmaykrishnapurachar@gmail.com>
2025-03-18 19:26:01 +0530

    rooms lights update now

--- app/(home)/Dashboard/Room.jsx
+++ app/(home)/Dashboard/Room.jsx
@@ -23,12 +23,32 @@ function Room({navigation , route}) {
                 light.name === name ? { ...light, state: !light.state } : light
             )
         );
-    };
-    
+    };    
+
+    useEffect(() => {
+        async function updateData(){
+            if(!lights)
+                return;
+            console.log("lights:", lights)
+            let t = 1;
+            const obj = lights.reduce((acc, light) => {
+                acc['light' + (t++)] = light
+                // console.log(acc);
+                return acc;
+            },{});
+
+            console.log("Object:",obj)
+
+            await update(ref(database, `${deviceId}/rooms/room${id}/lights`), obj).then(
+                console.log('updated')
+            )
+        } 
+        updateData();
+    }, [lights]);
 
     async function getData() {
         try {
-            let data = await get(ref(database, `${deviceId}/rooms/room${id}`));
+            let data = await get(ref(database, `/${deviceId}/rooms/room${id}`));
             setData(data.val());
         }catch(error) {
             console.log("Error occurred:" + error.message);
@@ -37,26 +57,27 @@ function Room({navigation , route}) {
 
     useEffect(() => {
         getData();
-        console.log(data);
+        console.log("Data:",data);
     }, []);
 
     useEffect(() => {
-        if(data) {
+        if(data && data.lights) {
             let length = Object.values(data.lights).length;
             let arr = [];
             for(let i = 1; i <= length; i++) {
-                console.log(data.lights['light' + i]);
+                // console.log(data.lights['light' + i]);
                 arr.push(data.lights['light' + i]);
             }
             setLights(arr);
-            console.log(arr);
+            // console.log(arr);
         }
     }, [data]);
 
     async function name() {
         try {
 
-            await update(ref(database, `/ef16bute/rooms/room1/lights`), {
+            await set(ref(database, `/ef16bute/rooms/room1`), {
+                lights: {
                     light1: {
                         name: "Light1",
                         state: true
@@ -65,9 +86,11 @@ function Room({navigation , route}) {
                         name: "Light2",
                         state: false
                     }
-            });
+                },
+                name: 'kitchen'
+            }).then(console.log('update'))
 
-            const snap = await get(ref(database, `/ef16bute/rooms/room1/lights`));
+            const snap = await get(ref(database, `/ef16bute/rooms/`));
             console.log(snap.val());
             
         } catch (error) {
